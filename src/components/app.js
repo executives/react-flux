e55aa1178e59f8ee153f86ddc58e443355fba1dd
unknown <pierceg83@gmail.com>
2017-10-10 13:42:10 -0700

    Mocks backend api; adds react-router; refactors routing

---
+++ src/components/app.js
@@ -0,0 +1,22 @@
+/*eslint-disable strict */ // Disabling check to handle the jQuery global var for Bootstrap
+
+var React = require('react');
+var Header = require('./common/header');
+var RouteHandler = require('react-router').RouteHandler;
+$ = jQuery = require('jquery');
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <Header />
+                <div className="container-fluid">
+                    <RouteHandler />
+                </div>
+                
+            </div>
+        );
+    }
+});
+
+module.exports = App;
